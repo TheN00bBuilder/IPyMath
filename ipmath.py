9f69518a84e4cbffc4d5f7992cf9557df785ac15
TheN00bBuilder <41171807+TheN00bBuilder@users.noreply.github.com>
2019-02-01 11:35:16 -0600

    New IP Math
    -Added `/32` and `/31` handling.
    -Made code a bit easier to read.

--- ipmath.py
+++ ipmath.py
@@ -10,18 +10,25 @@ args = parser.parse_args()
 
 bip = args.bip
 verbose = args.v
-
 if bip == None:
     print("Add an IP!")
 else:
     try:
-        address = ipaddress.IPv4Network(bip, strict=False)
+        address = ipaddress.IPv4Network(bip, strict = False)
+        edgecase = 'Usable Range: %s - %s' % (address[0], address[-1])
+        normalp = 'Usable Range: %s - %s' % (address[0]+1, address[-1]-1)
     except ValueError as e:
         print("This is not a valid IP range: %s" % e)
     else:
         if verbose == True:
             for ip in address:
                 print(ip)
-            print('Usable Range: %s - %s' % (address[0]+1, address[-1]-1))
+            if bip[-2:] == '32' or '31':
+                print(edgecase)
+            else:
+                print(normalp)
         else:
-            print('Usable Range: %s - %s' % (address[0]+1, address[-1]-1))
+                if bip[-2:] == '32' or '31':
+                    print(edgecase)
+                else:
+                    print(normalp)
