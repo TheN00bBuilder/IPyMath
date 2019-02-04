ff86efcf27e9ed47eabd9c6d6be68c1071cef175
TheN00bBuilder <41171807+TheN00bBuilder@users.noreply.github.com>
2019-02-04 11:35:25 -0600

    Changed from class to function.
    Also updated documentation.

--- bin/ipmath.py
+++ bin/ipmath.py
@@ -3,33 +3,32 @@
 import argparse
 import ipaddress
 
-class Domath():
-        def maths(bip, verbose):
-            """The whole thing is contained in one math class, I didn't feel the need to split it up. Calls builtin ipaddress function to do the math."""
+def domath(bip, verbose):
+    """The whole thing is contained in one math class, I didn't feel the need to split it up. Calls builtin ipaddress function to do the math."""
 
-            if bip == None:
-                print("Add an IP!")
-                pass
-            else:
-                try:
-                    address = ipaddress.IPv4Network(bip, strict = False)
-                    edgecase = 'Usable Range: %s - %s' % (address[0], address[-1])
-                    normalp = 'Usable Range: %s - %s' % (address[0]+1, address[-1]-1)
-                except ValueError as e:
-                    print("This is not a valid IP range: %s" % e)
+    if bip == None:
+        print("Add an IP!")
+        pass
+    else:
+        try:
+            address = ipaddress.IPv4Network(bip, strict = False)
+            edgecase = 'Usable Range: %s - %s' % (address[0], address[-1])
+            normalp = 'Usable Range: %s - %s' % (address[0]+1, address[-1]-1)
+        except ValueError as e:
+            print("This is not a valid IP range: %s" % e)
+        else:
+            if verbose == True:
+                for ip in address:
+                    print(ip)
+                if bip[-2:] == '32' or '31':
+                    print(edgecase)
                 else:
-                    if verbose == True:
-                        for ip in address:
-                            print(ip)
-                        if bip[-2:] == '32' or '31':
-                            print(edgecase)
-                        else:
-                            print(normalp)
-                    else:
-                        if bip[-2:] == '32' or '31':
-                            print(edgecase)
-                        else:
-                            print(normalp)
+                    print(normalp)
+            else:
+                if bip[-2:] == '32' or '31':
+                    print(edgecase)
+                else:
+                    print(normalp)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -40,4 +39,4 @@ if __name__ == "__main__":
     bip = args.bip
     verbose = args.v
     
-    Domath.maths(bip, verbose)
+    domath(bip, verbose)
