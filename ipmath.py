16525c84c2c6a774562fbc193607746da8dfd645
TheN00bBuilder <41171807+TheN00bBuilder@users.noreply.github.com>
2019-02-04 10:43:26 -0600

    ipmath, now with arguments AND methods!
    Yes, to all the 2 people reading this, I DID IT! I combined my arguments and methods into one single program. Next is a setup.py file and then we get to adding features!

--- ipmath.py
+++ ipmath.py
@@ -3,32 +3,41 @@
 import argparse
 import ipaddress
 
-parser = argparse.ArgumentParser()
-parser.add_argument("bip", type = str, help = "Put in your beginning IP with subnet here.")
-parser.add_argument("--v", action = 'store_true', help = "Set verbose to print all possible IP addresses with specified subnet. No flag will only print a range value.")
-args = parser.parse_args()
+class Domath():
+        def maths(bip, verbose):
+            """The whole thing is contained in one math class, I didn't feel the need to split it up. Calls builtin ipaddress function to do the math."""
 
-bip = args.bip
-verbose = args.v
-if bip == None:
-    print("Add an IP!")
-else:
-    try:
-        address = ipaddress.IPv4Network(bip, strict = False)
-        edgecase = 'Usable Range: %s - %s' % (address[0], address[-1])
-        normalp = 'Usable Range: %s - %s' % (address[0]+1, address[-1]-1)
-    except ValueError as e:
-        print("This is not a valid IP range: %s" % e)
-    else:
-        if verbose == True:
-            for ip in address:
-                print(ip)
-            if bip[-2:] == '32' or '31':
-                print(edgecase)
+            if bip == None:
+                print("Add an IP!")
+                pass
             else:
-                print(normalp)
-        else:
-                if bip[-2:] == '32' or '31':
-                    print(edgecase)
+                try:
+                    address = ipaddress.IPv4Network(bip, strict = False)
+                    edgecase = 'Usable Range: %s - %s' % (address[0], address[-1])
+                    normalp = 'Usable Range: %s - %s' % (address[0]+1, address[-1]-1)
+                except ValueError as e:
+                    print("This is not a valid IP range: %s" % e)
                 else:
-                    print(normalp)
+                    if verbose == True:
+                        for ip in address:
+                            print(ip)
+                        if bip[-2:] == '32' or '31':
+                            print(edgecase)
+                        else:
+                            print(normalp)
+                    else:
+                        if bip[-2:] == '32' or '31':
+                            print(edgecase)
+                        else:
+                            print(normalp)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("bip", type = str, help = "Put in your beginning IP with subnet here.")
+    parser.add_argument("--v", action = 'store_true', help = "Set verbose to print all possible IP addresses with specified subnet. No flag will only print a range value.")
+    args = parser.parse_args()
+
+    bip = args.bip
+    verbose = args.v
+    
+    Domath.maths(bip, verbose)
